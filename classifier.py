0f56d49b93961418bbc8f441097c290977bd58be
Tobi Lehman <mail@tobilehman.com>
2016-07-19 13:11:07 -0700

    Add generators and fit model!

--- classifier.py
+++ classifier.py
@@ -4,6 +4,8 @@
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
 
 # Build a model with 3 convolution layers, ReLu activation,
 # followed by max-pooling layers.
@@ -34,3 +36,34 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 
+# Randomly augment input images using this generator:
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    'data/train',  # this is the target directory
+    target_size=(150, 150),  # all images will be resized to 150x150
+    batch_size=32,
+    class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+    'data/validation',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    samples_per_epoch=2000,
+    nb_epoch=50,
+    validation_data=validation_generator,
+    nb_val_samples=800)
+
+model.save_weights('first_try.h5')  # always save your weights after training or during training
